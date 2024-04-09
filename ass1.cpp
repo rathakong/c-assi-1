9b6e7cc1099098f96c086f07192efab847444127
ratha <148026189+rathakong@users.noreply.github.com>
2024-04-09 18:24:08 +0700

    Update ass1.cpp

--- ass1.cpp
+++ ass1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
-int main() {
-    int a, b;
-    std::cout << "Put Numbers: ";
-    std::cin >> a >> b;
-    std::cout << "Total: " << a + b << std::endl;
-    return 0;
+int main(){
+	
+	int num1, num2, total;
+	std::cout <<"Put first number; ";
+	std::cin>> num1;
+	
+	std::cout<< "Put second number: ";
+	std::cin>> num2;
+	total = num1 + num2;
+	std::cout<<"Total: " << total <<
+	std::endl;
+	
+	return 0;
 }
