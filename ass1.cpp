168805c00b5869ef06b203c39af86920c28b89db
ratha <148026189+rathakong@users.noreply.github.com>
2024-04-09 18:08:03 +0700

    Add files via upload

---
+++ ass1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+	
+	int num1, num2, total;
+	std::cout <<"Enter first number; ";
+	std::cin>> num1;
+	
+	std::cout<< "Enter second number: ";
+	std::cin>> num2;
+	total = num1 + num2;
+	std::cout<<"Total: " << total <<
+	std::endl;
+	
+	return 0;
+}
