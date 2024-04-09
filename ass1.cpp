6360c6c8b35ca1006fa9af40d1e13749d03e63f5
ratha <148026189+rathakong@users.noreply.github.com>
2024-04-09 18:16:23 +0700

    Update ass1.cpp

--- ass1.cpp
+++ ass1.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 
-int main(){
-	
-	int num1, num2, total;
-	std::cout <<"Enter first number; ";
-	std::cin>> num1;
-	
-	std::cout<< "Enter second number: ";
-	std::cin>> num2;
-	total = num1 + num2;
-	std::cout<<"Total: " << total <<
-	std::endl;
-	
-	return 0;
+int main() {
+    int a, b;
+    std::cout << "Put Numbers: ";
+    std::cin >> a >> b;
+    std::cout << "Total: " << a + b << std::endl;
+    return 0;
 }
